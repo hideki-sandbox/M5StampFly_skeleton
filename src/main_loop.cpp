b83983b0246e18a56468caab6b15e243a331e00c
hideki-sandbox <highdeck123@gmail.com>
2025-08-30 17:07:57 +0900

    lesson1 回答を実装

--- src/main_loop.cpp
+++ src/main_loop.cpp
@@ -62,6 +62,8 @@ float limit(float value, float min, float max);
 void loop_400Hz(void) {
     // 400Hzで以降のコードが実行
 
+    StampFly.flag.mode = PARKING_MODE;
+
     update_loop400Hz();
     
     // Mode select
@@ -190,10 +192,10 @@ void flight_mode(void) {
     onboard_led2(YELLOW, 1);
     StampFly.ref.throttle = limit(Stick[THROTTLE], 0.0, 0.9);
 
-    motor_set_duty_fl(StampFly.ref.throttle);
-    motor_set_duty_fr(StampFly.ref.throttle);
-    motor_set_duty_rl(StampFly.ref.throttle);
-    motor_set_duty_rr(StampFly.ref.throttle);
+    motor_set_duty_fl(0.15);
+    motor_set_duty_fr(0.15);
+    motor_set_duty_rl(0.15);
+    motor_set_duty_rr(0.15);
 
     //Arm（スロットル）ボタンを監視して押されたらParkingモードに復帰するためのコード
     if (armButtonPressedAndRerleased)StampFly.flag.mode = PARKING_MODE;
